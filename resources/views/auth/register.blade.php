9cc927004b0b726976a6bdb4980c5083e29e7b6c
Diem <146576701+diemnq19@users.noreply.github.com>
2023-11-15 14:29:37 +0700

    fix_register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,20 @@
-<form method="POST" action="{{ route('logout') }}">
+<form method="POST" action="{{ route('register') }}">
     @csrf
-    <button type="submit">Logout</button>
+
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name" required>
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" required>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password" required>
+
+    <label for="address">Address:</label>
+    <input type="text" name="address" id="address" required>
+
+    <label for="phone">Phone:</label>
+    <input type="text" name="phone" id="phone" required>
+
+    <button type="submit">Register</button>
 </form>

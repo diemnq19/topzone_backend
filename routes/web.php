0c2fc69b59cc80ad21e6dcbc93c9a2e87b59127e
Diem <146576701+diemnq19@users.noreply.github.com>
2023-11-15 14:43:59 +0700

    change_api_file

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,4 @@ use App\Http\Controllers\OrderController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::post('/login', [App\Http\Controllers\Auth\LoginController::class,'login'])->name('login');
-Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register'])->name('register');
-Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
-Route::get('/login/index', [App\Http\Controllers\Auth\LoginController::class, 'index']);
-Route::middleware(['auth'])->group(function () {
-    Route::get('/user',  [App\Http\Controllers\UserController::class, 'user'])->name('user');
-});
-Route::resource('brands', BrandController::class);
-Route::resource('news', NewsController::class);
-Route::resource('reviews', ReviewController::class);
-Route::resource('products', ProductController::class);
-Route::resource('shopping-carts', ShoppingCartController::class);
-Route::resource('orders', OrderController::class);
